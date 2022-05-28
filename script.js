1456a927892eedee806719585cbda22c43de5506
Leckar <gusleckar@gmail.com>
2022-05-28 14:53:35 -0300

    completed up to requisite 4

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
+// Variáveis necessárias para o funcionamento da aplicação;
 const rgbText = document.getElementById('rgb-color');
+const ballsParent = document.getElementById('color-balls');
+const balls = document.getElementsByClassName('ball');
+const correctColor = document.getElementById('correct-color');
 // Gerador dos valores aleatórios do rgb;
 function randomRGBNumber() {
   return Math.round(Math.random() * 230);
@@ -11,4 +15,40 @@ function randomRGBColor() {
 function rgbCodeSetter() {
 	rgbText.innerText = randomRGBColor();
 }
-rgbCodeSetter();
+// Função que atribui aleatoriamente a um dos círculos a classe 'correct-color';
+function correctColorRandomizer() {
+	if (correctColor) {
+		correctColor.removeAttribute('id');
+	}
+	balls[Math.floor(Math.random() * 5)].setAttribute('id', 'correct-color')
+}
+// Função que gera as cores dos círculos;
+function circleFilling() {
+	correctColorRandomizer()
+	for (let i = 0; i < balls.length; i += 1) {
+		if (balls[i] === correctColor) {
+			balls[i].style.backgroundColor = `${rgbText.innerText}`;
+		} else {
+			balls[i].style.backgroundColor = randomRGBColor();
+		}
+	}
+}
+// Função que gera os círculos que serão coloridos;
+function createCircle() {
+	const newCircle = document.createElement('div');
+	newCircle.classList.add('ball');
+	return newCircle;
+}
+// Função que chama a criação dos círculos e os alinha;
+function circleLine() {
+	for (let i = 1; i <= 6; i += 1) {
+		const circle = createCircle();
+		ballsParent.appendChild(circle);
+	}
+	circleFilling()
+}
+
+window.onload = () => {
+	rgbCodeSetter();
+	circleLine();
+};
